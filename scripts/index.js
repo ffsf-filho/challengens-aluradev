f5f78e402f6502e9f8b47349e2e5e6e2d51f8fc1
Francisco Florentino de Sousa Filho <ffsf.filho@gmail.com>
2021-05-23 00:53:02 -0300

    Implementado a responsividade para tablet max-width: 768px

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,19 @@ function corBackground(valor){
   let divMoldura = document.getElementById('divEditorCodigoMoldura')
   divMoldura.style.backgroundColor = valor
 }
+
+function exibeMenuPerfil(visivel = false){
+  let divHeaderMenuPerfil = document.getElementById('headerMenuPerfil')
+  let imgIconeHamburger = document.getElementById('imgIconeHamburger')
+  let imgIconeFechar =  document.getElementById('imgIconeFechar')
+
+  if(visivel){
+    imgIconeHamburger.setAttribute("hidden", "true")
+    imgIconeFechar.removeAttribute("hidden")
+    divHeaderMenuPerfil.className = "header__menuPerfil exibeMenu"
+  } else {
+    imgIconeHamburger.removeAttribute('hidden')
+    imgIconeFechar.setAttribute("hidden", "true")
+    divHeaderMenuPerfil.className = "header__menuPerfil hideMenu" 
+  }
+}
